867e50fa3520d9783e60ae59eea12b1f331111fb
Mikey <terry.bazemoreiii@gmail.com>
2021-03-01 15:30:19 -0500

    ✨added password generation

--- cogs/security.py
+++ cogs/security.py
@@ -1,7 +1,6 @@
 from discord.ext import commands
 import discord
 import secrets
-import hashlib
 import string
 import aiofiles
 
@@ -17,15 +16,20 @@ class security(commands.Cog):
         pwd = ''.join(secrets.choice(chars) for i in range(length))
         embed=discord.Embed(color=self.bot.color)
         embed.add_field(name="your password",value=f'||`{pwd}`||')
+        await ctx.send("check your dms!")
+        await ctx.author.send(embed=embed)
 
     @commands.command('xkcdpassgen',description="generates an [xkcd style](https://xkcd.com/936/) password")
     async def xkcdpassgen(self, ctx:commands.Context, length=4):
         if length > 15:
             return await ctx.send("That's too long!")
-        async with aiofiles.open('words.txt','r') as f:
-            words = [word.strip() for word in f]
+        async with aiofiles.open('cogs\words.txt','r') as f:
+            words = [word.strip() for word in await f.readlines()]
             pwd = ''.join(secrets.choice(words) for i in range(length))
-
+        embed=discord.Embed(color=self.bot.color)
+        embed.add_field(name="your password",value=f'||`{pwd}`||')
+        await ctx.send("check your dms!")
+        await ctx.author.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(security(bot))
